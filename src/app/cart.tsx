228afbfb062c1ac65111328f0f3a545f98b9522a
Karen Dantas <karennobre74@gmail.com>
2024-07-06 15:14:34 -0300

    feat: create function to remove product

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,8 +1,13 @@
+import { Button } from "@/components/button";
 import { Header } from "@/components/header";
+import { Input } from "@/components/input";
 import { Product } from "@/components/product";
 import { useCartStore } from "@/stores/cart-stores";
+import { ProductProps } from "@/utils/data/products";
 import { FormatCurrency } from "@/utils/functions/format-currency";
-import { ScrollView, Text, View } from "react-native";
+import { Link } from "expo-router";
+import { Alert, ScrollView, Text, View } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 
 export default function Cart (){
@@ -12,19 +17,32 @@ export default function Cart (){
         total + product.price * product.quantity
     , 0))
 
+    function handleProductRemove (product: ProductProps){
+        Alert.alert("Remover", `Deseja remover ${product.title}?`, [
+            {
+                text : "Cancelar",
+            },
+            {
+                text: "Remover",
+                onPress: () => cartStore.remove(product.id)
+            },
+        ])
+    }
     return(
         <View className="flex-1 pt-8">
             <Header title = "seu carrinho"/>
+            <KeyboardAwareScrollView>
             <ScrollView> 
             {
                 cartStore.products.length > 0 ? (
-         
                     <View className="p-5 flex-1">
+         
                     {
                         cartStore.products.map((product) => (
                             <Product 
                                 key = {product.id}
                                 data = {product}
+                                onPress={() => handleProductRemove(product)}
                             />
                         ))
                     }
@@ -40,8 +58,19 @@ export default function Cart (){
                 </Text>
 
                 <Text className="text-lime-400 text-2xl font-heading"> {total}</Text>
+
             </View>
+                <Input placeholder="Informe o endereço de entrega com rua, bairro, CEP, número e complemento..." />
             </ScrollView>
+            </KeyboardAwareScrollView>
+
+            <View className="p-5 gap-5">
+                <Button>
+                    <Button.Text> Enviar pedido</Button.Text>
+                </Button>
+
+                <Link href={"/"}> Voltar ao cardápio </Link>
+            </View>
         </View>
     )
 }
--- src/stores/helpers/cart-in-memory.ts
+++ src/stores/helpers/cart-in-memory.ts
@@ -16,3 +16,18 @@ export function add (products: ProductCartProps[], newProduct: ProductProps){
     //se é um novo produto, retornamos uma nova lista com o novo produto
     return [...products, {...newProduct, quantity: 1}]
 }
+
+export function remove (products: ProductCartProps[], productRemovedId: string){
+    //encontrando o produto que sera removido, quando a quantidade for menor que 1 ele será zerado
+    const updatedProducts = products.map((product) => product.id === productRemovedId 
+        ? {
+            ...product, 
+            quantity: product.quantity > 1 ? product.quantity -1 : 0
+        } 
+        : product
+    )
+
+    //limpando os produtos que possuem quantidade 0 do carrinho
+    return updatedProducts.filter((product) => product.quantity > 0)
+
+}
